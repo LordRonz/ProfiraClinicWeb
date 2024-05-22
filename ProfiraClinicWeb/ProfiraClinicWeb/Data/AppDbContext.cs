9202095f52eb70ef6e4bf7119fee5bef010147d0
LordRonz <aarontanhar2000@gmail.com>
2024-05-23 00:15:33 +0700

    feat: add entity framework core

---
+++ ProfiraClinicWeb/ProfiraClinicWeb/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Excubo.Blazor.Canvas.Contexts;
+using Microsoft.EntityFrameworkCore;
+using ProfiraClinicWeb.Models;
+using System.Collections.Generic;
+
+namespace ProfiraClinicWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public IConfiguration _config { get; set; }
+        public AppDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<MKary> MKary { get; set; }
+    }
+}
