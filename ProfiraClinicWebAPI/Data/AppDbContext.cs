7559b198230308811605c42a04bf15a2b06b4efa
LordRonz <aarontanhar2000@gmail.com>
2025-02-20 00:44:59 +0700

    feat: webapi profiraclinic

---
+++ ProfiraClinicWebAPI/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace ProfiraClinicWebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public IConfiguration _config { get; set; }
+        public AppDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
