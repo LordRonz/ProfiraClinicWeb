c4a02f281384db28959a926897b343aafccf9434
LordRonz <aarontanhar2000@gmail.com>
2025-02-23 00:41:57 +0700

    feat: pasien controller

---
+++ ProfiraClinicWebAPI/Model/Dokter.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProfiraClinicWebAPI.Model
+{
+    public class MKARY
+    {
+        [Key]
+        public string? UserName { get; set; }
+        public string? UserPassword { get; set; }
+        public string KDKAR { get; set; }
+        public string NMKAR { get; set; }
+        public DateTime? TGMSK { get; set; }
+        public string ALMT1 { get; set; }
+        public string KOTA1 { get; set; }
+        public string TELP1 { get; set; }
+        public string ALMT2 { get; set; }
+        public string KOTA2 { get; set; }
+        public string TELP2 { get; set; }
+        public string KOTAL { get; set; }
+        public DateTime? TGLLH { get; set; }
+        public string AKTIF { get; set; }
+        public string USRID { get; set; }
+        public DateTime? UPDDT { get; set; }
+    }
+}
