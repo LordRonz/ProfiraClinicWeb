c7374be96507d03837a44d09c328c51adb58f905
LordRonz <aarontanhar2000@gmail.com>
2025-04-22 00:47:36 +0700

    feat: forms and user group

---
+++ Model/Core/UserGroup.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProfiraClinic.Models.Core
+{
+    public class UserGroup
+    {
+
+        [Column(TypeName = "char(10)")]
+        [MaxLength(10)]
+        public string KodeUserGroup { get; set; }
+
+        [Column(TypeName = "varchar(50)")]
+        [MaxLength(50)]
+        public string NamaUserGroup { get; set; }
+
+        public DateTime? UPDDT { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        [MaxLength(10)]
+        public string USRID { get; set; }
+    }
+}
