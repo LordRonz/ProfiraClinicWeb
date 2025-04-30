61adf4137c6ac9e2afd181dbb75b360788a41426
LordRonz <aarontanhar2000@gmail.com>
2025-05-01 00:06:29 +0700

    feat: base crud controller

---
+++ ProfiraClinicWebAPI/Controllers/BaseCrudController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProfiraClinicWebAPI.Data;
+using ProfiraClinicWebAPI.Helper;
+
+namespace ProfiraClinicWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseCrudController<TEntity> : ControllerBase
+    where TEntity : class
+    {
+        protected readonly AppDbContext _context;
+
+        protected BaseCrudController(AppDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        /// <summary>
+        /// The DbSet for this entity
+        /// </summary>
+        protected abstract DbSet<TEntity> DbSet { get; }
+
+        /// <summary>
+        /// Apply your EF.Functions.Like search here
+        /// </summary>
+        protected abstract IQueryable<TEntity> ApplySearch(
+            IQueryable<TEntity> query,
+            string likeParam);
+
+        /// <summary>
+        /// Apply ordering after search
+        /// </summary>
+        protected abstract IOrderedQueryable<TEntity> ApplyOrder(
+            IQueryable<TEntity> query);
+
+        [HttpGet]
+        public virtual async Task<ActionResult<IEnumerable<TEntity>>> GetItems()
+            => await DbSet.ToListAsync();
+
+        [HttpGet("{id}")]
+        public virtual async Task<ActionResult<TEntity>> GetItem(string id)
+        {
+            var item = await DbSet.FindAsync(id);
+            return item == null ? NotFound() : item;
+        }
+
+        [HttpPost("search")]
+        public virtual async Task<ActionResult<List<TEntity>>> Search(
+            [FromBody] BaseBodyListOr body)
+        {
+            var q = ApplySearch(DbSet, body.GetParam);
+            q = ApplyOrder(q);
+            return await q.ToListAsync();
+        }
+    }
+}
