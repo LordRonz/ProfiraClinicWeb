07923d7af504e8af7ddff0cf111455300d616c06
LordRonz <aarontanhar2000@gmail.com>
2025-06-24 01:27:08 +0700

    feat: appointment core model

--- Model/Core/Appointment.cs
+++ Model/Core/Appointment.cs
@@ -17,10 +17,11 @@ namespace ProfiraClinic.Models.Core
         [Column(TypeName = "char(2)")]
         public string? BulanTransaksi { get; set; }
 
-        public DateTime? TgFak { get; set; }
+        public DateTime? TanggalTransaksi { get; set; }
 
+        [Key]
         [Column(TypeName = "char(15)")]
-        public string? NomorAppointment { get; set; }
+        public string NomorAppointment { get; set; }
 
         public DateTime? TanggalAppointment { get; set; }
         public DateTime? JamAppointment { get; set; }
@@ -28,9 +29,6 @@ namespace ProfiraClinic.Models.Core
         [Key]
         [Column(TypeName = "nchar(10)")]
         public string? KodeKaryawan { get; set; }
-
-        [Column(TypeName = "char(1)")]
-        public string? InCus { get; set; }
 
         [Column(TypeName = "nchar(10)")]
         public string? KodeCustomer { get; set; }
@@ -42,16 +40,10 @@ namespace ProfiraClinic.Models.Core
         public string? NomorHP { get; set; }
 
         [Column(TypeName = "varchar(50)")]
-        public string? Tindakan { get; set; }
-
-        [Column(TypeName = "char(1)")]
-        public string? InKat { get; set; }
+        public string? KodeRuangan { get; set; }
 
         [Column(TypeName = "varchar(50)")]
         public string? Keterangan { get; set; }
-
-        [Column(TypeName = "char(1)")]
-        public string? InApp { get; set; }
 
         public DateTime? Reception { get; set; }
         public DateTime? Perawatan { get; set; }
@@ -63,6 +55,9 @@ namespace ProfiraClinic.Models.Core
 
         [Column(TypeName = "char(1)")]
         public string? StatusAppointment { get; set; }
+
+        [Column(TypeName = "int")]
+        public int Estimasi { get; set; }
 
         public DateTime? UpdDt { get; set; }
 
