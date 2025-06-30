282c0b1d0090ee1dcab825d67d019ca2e7c5e433
LordRonz <aarontanhar2000@gmail.com>
2025-07-01 01:08:19 +0700

    feat: appointment api

--- ProfiraClinicWebAPI/Controllers/AppointmentController.cs
+++ ProfiraClinicWebAPI/Controllers/AppointmentController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using ProfiraClinic.Models.Core;
 using ProfiraClinicWebAPI.Data;
@@ -6,9 +7,18 @@ using ProfiraClinicWebAPI.Helper;
 
 namespace ProfiraClinicWebAPI.Controllers
 {
+
     public class AppointmentController
     : BaseCrudController<Appointment>
     {
+
+        public class AppointmentListDto()
+        {
+            public string KodeLokasi {   get; set; }
+            public DateTime? TanggalAppointment { get; set; }
+            public string KodeKaryawan { get; set; }
+        }
+
         public AppointmentController(AppDbContext ctx) : base(ctx) { }
 
         protected override DbSet<Appointment> DbSet
@@ -22,10 +32,30 @@ namespace ProfiraClinicWebAPI.Controllers
                 || EF.Functions.Like(d.KodeCustomer, likeParam)
             || EF.Functions.Like(d.KodeLokasi, likeParam)
             || EF.Functions.Like(d.KodeRuangan, likeParam)
-            || EF.Functions.Like(d.NomorHP, likeParam));
+            );
 
         protected override IOrderedQueryable<Appointment> ApplyOrder(
             IQueryable<Appointment> q)
             => q.OrderBy(d => d.TanggalAppointment);
+
+        [HttpPost("GetListDokter")]
+        public async Task<IActionResult> GetListDokter([FromBody] AppointmentListDto appDto)
+        {
+            var sqlParameters = new[]
+            {
+        new SqlParameter("@KodeLokasi", appDto.KodeLokasi ?? (object)DBNull.Value),
+
+        new SqlParameter("@TanggalAppointment", appDto.TanggalAppointment ?? (object)DBNull.Value),
+
+        new SqlParameter("@KodeKaryawan", appDto.KodeKaryawan ?? (object)DBNull.Value),
+
+    };
+
+            var list = await _context.Appointment
+                .FromSqlRaw("EXEC dbo.usp_TRM_Appointment_List @KodeLokasi, @TanggalAppointment, @KodeKaryawan", sqlParameters)
+                .ToListAsync();
+
+            return Ok(list);
+        }
     }
 }
