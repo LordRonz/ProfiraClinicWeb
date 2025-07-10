57bdd3b54bf945838561b2cbac5d78dccedf0602
Youngky H <youngky_h@yahoo.co.id>
2025-07-10 13:32:49 +0700

    ProfiraClinicRME: login UI

---
+++ ProfiraClinicRME/Model/ServiceResult.cs
@@ -0,0 +1,22 @@
+﻿namespace ProfiraClinicRME.Model
+{
+
+    public enum ServiceResultEnum
+    {
+        FOUND,
+        NOT_FOUND,
+        SUCCESS,
+        FAIL
+    }
+
+    /// <summary>
+    /// Generic service result
+    /// </summary>
+    /// 
+    public class ServiceResult<T>
+    {
+        public ServiceResultEnum Status;
+        public string Message;
+        public T? Data;
+    }
+}
