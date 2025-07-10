91f31e8b35e19fcc3c6f0da3cd7bcfbf5844d53e
Youngky H <youngky_h@yahoo.co.id>
2025-07-10 13:29:42 +0700

    ProfiraClinicRme: AuthService

---
+++ Model/Api/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProfiraClinic.Models.Api
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string? KodeLokasi { get; set; }
+    }
+}
