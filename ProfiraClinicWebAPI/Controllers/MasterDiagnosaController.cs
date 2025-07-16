1019e08e805cccb07b2d2dfca3717f638a897657
LordRonz <aarontanhar2000@gmail.com>
2025-07-17 00:31:45 +0700

    feat: master diagnosa

---
+++ ProfiraClinicWebAPI/Controllers/MasterDiagnosaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProfiraClinic.Models.Core;
+using ProfiraClinicWebAPI.Data;
+
+namespace ProfiraClinicWebAPI.Controllers
+{
+    public class MasterDiagnosaController
+    : BaseCrudController<MasterDiagnosa>
+    {
+        public MasterDiagnosaController(AppDbContext ctx) : base(ctx) { }
+
+        protected override DbSet<MasterDiagnosa> DbSet
+            => _context.MasterDiagnosa;
+
+        protected override IQueryable<MasterDiagnosa> ApplySearch(
+            IQueryable<MasterDiagnosa> q,
+            string likeParam)
+            => q.Where(d
+                => EF.Functions.Like(d.KodeDiagnosa, likeParam)
+                || EF.Functions.Like(d.NamaDiagnosa, likeParam));
+
+        protected override IOrderedQueryable<MasterDiagnosa> ApplyOrder(
+            IQueryable<MasterDiagnosa> q)
+            => q.OrderBy(d => d.UPDDT);
+    }
+}
