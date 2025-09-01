4e5055e681bb4b5de26d434defc8fa2ffa708381
Youngky H <youngky_h@yahoo.co.id>
2025-09-01 10:23:31 +0700

    App::PenandaanGambar: sync edited canvas to markedImage

---
+++ ProfiraClinicRME/Utils/Util.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace ProfiraClinicRME.Utils
+{
+    public class OpStatus
+    {
+        public int Status { get; set; } = 1;
+        public string Message { get; set; } = "";
+
+        public object Data { get; set; } // Use JsonElement for manual parsing
+
+    }
+
+    public static class JsInvoke
+    {
+
+    }
+
+    public class Util
+    {
+        public static string GenerateRandomAlphanumeric(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
