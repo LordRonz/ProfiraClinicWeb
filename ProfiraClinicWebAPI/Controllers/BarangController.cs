0f5c18a0a6e33aca6abf5a274424ae7c6f09ed27
LordRonz <aarontanhar2000@gmail.com>
2025-09-16 01:15:58 +0700

    feat: item product 2

--- ProfiraClinicWebAPI/Controllers/BarangController.cs
+++ ProfiraClinicWebAPI/Controllers/BarangController.cs
@@ -1,31 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using ProfiraClinic.Models.Api;
 using ProfiraClinic.Models.Core;
 using ProfiraClinicWebAPI.Data;
 
 namespace ProfiraClinicWebAPI.Controllers
 {
     public class BarangController
-    : BaseCrudController<Barang>
+        : BaseCrudController<Barang>
     {
         public BarangController(AppDbContext ctx) : base(ctx) { }
 
-        protected override DbSet<Barang> DbSet
-            => _context.Barang;
+        protected override DbSet<Barang> DbSet => _context.Barang;
 
-        protected override IQueryable<Barang> ApplySearch(
-            IQueryable<Barang> q,
-            string likeParam)
-            => q.Where(d
-                => EF.Functions.Like(d.KodeBarang, likeParam)
-            || EF.Functions.Like(d.KodeBarang, likeParam));
+        protected override IQueryable<Barang> ApplySearch(IQueryable<Barang> q, string likeParam)
+            => q.Where(d =>
+                EF.Functions.Like(d.KodeBarang, likeParam) ||
+                EF.Functions.Like(d.NamaBarang, likeParam));
 
-        protected override IOrderedQueryable<Barang> ApplyOrder(
-            IQueryable<Barang> q)
+        protected override IOrderedQueryable<Barang> ApplyOrder(IQueryable<Barang> q)
             => q.OrderBy(d => d.KodeBarang);
 
         [NonAction]
-        public override Task<ActionResult> GetItems(string last = null, [FromQuery] int page = 1, [FromQuery] int pageSize = 20 )
+        public override Task<ActionResult> GetItems(string last = null, [FromQuery] int page = 1, [FromQuery] int pageSize = 20)
             => base.GetItems(last);
 
         [HttpGet("GetList")]
