0ee80c9604f5ceefff899c207c6705fb86a60271
LordRonz <aarontanhar2000@gmail.com>
2025-10-14 01:21:49 +0700

    feat: user group otorisasi

---
+++ Model/Core/UserGroupOtorisasi.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProfiraClinic.Models.Core
+{
+    [Keyless]
+    [Table("MUserGroupOtorisasi")]
+    public class UserGroupOtorisasi
+    {
+
+        [Column(TypeName = "char(5)")]
+        [MaxLength(5)]
+        public string KodeAplikasi { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        [MaxLength(10)]
+        public string KodeUserGroup { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        [MaxLength(10)]
+        public string KodeModul { get; set; }
+
+        public DateTime? UPDDT { get; set; }
+
+        [Column(TypeName = "char(10)")]
+        [MaxLength(10)]
+        public string USRID { get; set; }
+    }
+}
